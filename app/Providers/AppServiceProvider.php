e31db2410881a2cca8189dd5c7122a9c69572da8
FrancoDebord <fhoueha@gmail.com>
2025-11-05 08:44:47 +0100

    airid

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\AIRID_Departement;
+use App\Models\AIRID_Partenaire;
 use App\Models\Departement;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,9 +25,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+         if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
         
          // $departements_menu = [];
         $departements_menu = AIRID_Departement::where("afficher_menu",1)->get();
         view()->share("departements_menu",$departements_menu);
+
+           $all_partenaires = AIRID_Partenaire::all();
+        view()->share("all_partenaires",$all_partenaires);
     }
 }
